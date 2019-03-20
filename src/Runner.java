d120362f85f13100d92ed00d80ef5d014d61bc18
codytheking <codytheking93@gmail.com>
2019-03-20 13:29:22 -0700

    Indices were wrong

--- src/Runner.java
+++ src/Runner.java
@@ -17,17 +17,17 @@ public class Runner
 		
 		// Rectangle
 		sides.set(0, 6.5);
-		sides.set(0, 6.5);
-		sides.set(0, 3.0);
-		sides.set(0, 3.0);
+		sides.set(1, 6.5);
+		sides.set(2, 3.0);
+		sides.set(3, 3.0);
 		Polygon p1 = new Rectangle(sides1);
 		System.out.println(p1);
 		
 		// Square
 		sides.set(0, 10.2);
-		sides.set(0, 10.2);
-		sides.set(0, 10.2);
-		sides.set(0, 10.2);
+		sides.set(1, 10.2);
+		sides.set(2, 10.2);
+		sides.set(3, 10.2);
 		Polygon p2 = new Rectangle(sides1);
 		System.out.println(p2);
 	}
