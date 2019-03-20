2941564c561dd04f1a7c7eadd447c2c28546b791
codytheking <codytheking93@gmail.com>
2019-03-20 12:09:49 -0700

    Add Runner for testing

---
+++ src/Runner.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner 
+{
+	public static void main(String[] args)
+	{
+		List<Double> sides = new ArrayList<Double>();
+		sides.add(3.0);
+		sides.add(4.5);
+		sides.add(5.5);
+		sides.add(4.75);
+		
+		// Quadrilateral
+		Polygon p = new Quadrilateral(sides, 30.0, 25.0);
+		System.out.println(p);
+		
+		// Rectangle
+		sides.set(0, 6.5);
+		sides.set(0, 6.5);
+		sides.set(0, 3.0);
+		sides.set(0, 3.0);
+		Polygon p1 = new Rectangle(sides1);
+		System.out.println(p1);
+		
+		// Square
+		sides.set(0, 10.2);
+		sides.set(0, 10.2);
+		sides.set(0, 10.2);
+		sides.set(0, 10.2);
+		Polygon p2 = new Rectangle(sides1);
+		System.out.println(p2);
+	}
+}
